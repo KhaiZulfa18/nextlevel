3901962a804fee6991cfaa06c71a104972c839aa
KhaiZulfa18 <khaizulfa18@gmail.com>
2024-09-04 23:49:40 +0700

    fix theme toggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -2,14 +2,22 @@
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 import { IconSun, IconMoon } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
 
     const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
 
+    useEffect(() => {
+        setMounted(true);
+    }, [])
+
+    if(!mounted) return null;
+    
     return (
         <div className="flex gap-3 justify-end">
-            <Button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+            <Button variant={'outline'} onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
                 {theme === 'dark' ? <IconSun size={16}/> : <IconMoon size={16}/>}
             </Button>
         </div>
