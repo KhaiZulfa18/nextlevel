f609b788fed5c913021851a9bda83a34ea625f2b
KhaiZulfa18 <khaizulfa18@gmail.com>
2024-09-22 17:41:10 +0700

    install tanstack react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider"
+import { QueryProvider } from "@/components/provider/query-provider"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,13 +19,15 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <body className={inter.className}>
-                <ThemeProvider 
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
-                    disableTransitionOnChange>
-                    {children}
-                </ThemeProvider>
+                <QueryProvider>
+                    <ThemeProvider 
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange>
+                        {children}
+                    </ThemeProvider>
+                </QueryProvider>
             </body>
         </html>
   );
