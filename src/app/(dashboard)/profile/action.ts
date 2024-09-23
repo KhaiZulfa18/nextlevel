5163d80ca56103326daf0d0f35241a23b16f6095
KhaiZulfa18 <khaizulfa18@gmail.com>
2024-09-23 23:46:52 +0700

    add update password and email

--- src/app/(dashboard)/profile/action.ts
+++ src/app/(dashboard)/profile/action.ts
@@ -2,6 +2,8 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
+import { z } from "zod";
+import bcrypt from 'bcryptjs';
 
 export async function getUserProfile() {
 
@@ -55,6 +57,98 @@ export async function updateProfile(prevState: FormData,formData: FormData): Pro
     revalidatePath('/profile');
     return { status: 201, message: 'Profile updated successfully.' };
 }
+
+export async function updatePassword(prevState: FormData,formData: FormData): Promise<ActionResult> {
+
+    const { user } = await validateRequest();
+    const userId = user?.id
+
+    const currentPassword = formData.get("current_password")?.toString() || '';
+    const confirmPassword = formData.get("confirm_new_password")?.toString() || '';
+    const newPassword = formData.get("new_password")?.toString();
+    const email = formData.get("email")?.toString();
+
+    // check validation 
+    const validationEmail = userSchema.safeParse({
+        email: email,
+        password: newPassword,
+        confirmPassword: confirmPassword
+    });
+
+    if(!validationEmail.success) {
+        const errorMessage = validationEmail.error.issues[0].message;
+        return { status: 400, message: errorMessage };
+    }    
+
+    // check email is already used on another user
+    const emailExists = await prisma.user.findMany({
+        where: {
+            email: email,
+            NOT: {
+                id: userId
+            }
+        }
+    });
+
+    if(emailExists.length > 0) {
+        return { status: 400, message: 'Email has already taken, please use another email.' };
+    }
+
+    // find user to get current password status
+    const getUser = await prisma.user.findUnique({
+        where: {
+            id: userId
+        }
+    });
+
+    // if user password is already set
+    if(getUser?.password) {
+
+        // check if the current password is correct
+        const validPassword = await bcrypt.compare(currentPassword, getUser.password);
+
+        if(!validPassword) {
+            return { status: 400, message: 'Current password is incorrect. Please try again.' };
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        const updatePassword = await prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                password: hashedPassword,
+                email: email,
+            }
+        });
+    } else {
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        const updatePassword = await prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                password: hashedPassword,
+                email: email,
+            }
+        });
+    }
+
+    revalidatePath('/profile');
+    return { status: 201, message: 'Email & Password updated successfully.' };
+}
+
+const userSchema = z.object({
+    email: z.string().email({ message: 'Invalid email address' }),
+    password: z.string().min(5, { message: 'Password must be at least 5 characters long' }),
+    confirmPassword: z.string().min(1, { message: 'Confirm password is required' }),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: 'Your password and confirmation don\'t match. Please try again.',
+    path: ['confirmPassword'],
+});
 
 interface ActionResult {
     status: number;
--- src/app/(dashboard)/profile/updatePassword.tsx
+++ src/app/(dashboard)/profile/updatePassword.tsx
@@ -5,18 +5,19 @@ import { Label } from "@/components/ui/label";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import SubmitButton from "@/components/form/SubmitButton";
 import { useFormState } from "react-dom";
-import { updateProfile } from "./action";
+import { updatePassword } from "./action";
 import { IconCheck, IconX } from "@tabler/icons-react";
 
 export default function UpdatePassword({user} : {user: any}) {
 
-    const [state, formAction] = useFormState(updateProfile, {
+    const [state, formAction] = useFormState(updatePassword, {
         status: '',
         message: '',
     });
 
     return (
-            <Card className="w-full bg-background">
+        <Card className="w-full bg-background">
+            <form action={formAction} autoComplete="off">
             <CardHeader>
                 <CardTitle>Change Email & Password</CardTitle>
                 <CardDescription>Update your email and password.</CardDescription>
@@ -24,29 +25,40 @@ export default function UpdatePassword({user} : {user: any}) {
             <CardContent className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div className="space-y-2">
                     <Label htmlFor="email">Email</Label>
-                    <Input id="email" type="email" placeholder="Email" defaultValue={user?.email} />
+                    <Input name="email" type="email" placeholder="Email" defaultValue={user?.email || ''} autoComplete="nope"/>
+                    {!user.email && (
+                        <span className="text-gray-500 text-sm">No email detected. Please enter your email to use email and password login.</span>
+                    )}
                 </div>
                 {user?.password ? (
                     <>
                     <div className="space-y-2">
                         <Label htmlFor="current_password">Current Password</Label>
-                        <Input id="current_password" type="password" placeholder="Current Password" autoComplete="off" />
+                        <Input name="current_password" type="password" placeholder="Current Password" autoComplete="off" />
                     </div>
                     <div className="space-y-2">
                         <Label htmlFor="new_password">New Password</Label>
-                        <Input id="new_password" type="password" placeholder="New Password" />
+                        <Input name="new_password" type="password" placeholder="New Password" />
                     </div>
                     <div className="space-y-2">
                         <Label htmlFor="confirm_new_password">Confirm New Password</Label>
-                        <Input id="confirm_new_password" type="password" placeholder="Confirm New Password" />
+                        <Input name="confirm_new_password" type="password" placeholder="Confirm New Password" />
                     </div>
                     </>
                 ) : (
+                    <>
                     <div className="space-y-2">
-                        <Label htmlFor="password">Set Password</Label>
-                        <Input id="password" type="password" placeholder="Password" />
+                        <Label htmlFor="new_password">Set New Password</Label>
+                        <Input name="new_password" type="password" placeholder="Password" />
                         <span className="text-gray-500 text-sm">You can set a password for this account to use email and password login in the future.</span>
                     </div>
+                    <div className="space-y-2">
+                        <Label htmlFor="confirm_new_password">Confirm New Password</Label>
+                        <Input name="confirm_new_password" type="password" placeholder="Confirm New Password" />
+                    </div>
+                    <div className="space-y-2">
+                    </div>
+                    </>
                 )}
                 {state.message && 
                     <Alert variant={ state.status === 201 ? 'success' : 'destructive'} className="">
@@ -61,6 +73,7 @@ export default function UpdatePassword({user} : {user: any}) {
             <CardFooter>
                 <SubmitButton text="Save Email & New Password" />
             </CardFooter>
+            </form>
         </Card>
     )
 }
