1cc2e4602725420999803bb373d84dee0dd6beb9
KhaiZulfa18 <khaizulfa18@gmail.com>
2024-10-08 22:41:03 +0700

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,22 @@ import Link from "next/link";
 import Navbar from "@/components/layout/navbar";
 import { Button } from "@/components/ui/button";
 import { IconBrandGithub, IconRocket } from "@tabler/icons-react";
+import Footer from "@/components/layout/footer";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
 
     return (
         <div className="flex min-h-screen w-full flex-col">
             <Navbar/>
-            <div className="h-full w-full flex items-center justify-center">
+            <div className="h-full w-full flex items-center flex-col justify-center">
                 <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_70%_50%_at_50%_00%,#000_70%,transparent_110%)]">
                 </div>
-                <div className="z-10 text-center py-24">
+                <div className="z-10 text-center py-16 md:py-24 px-8">
                     <h1 className="text-4xl sm:text-5xl md:text-6xl font-mono font-extrabold tracking-tight mb-6">
                         NextLevel
                     </h1>
-                    <p className="text-xl sm:text-2xl mb-8 max-w-3xl mx-auto font-mono">
+                    <p className="text-md md:text-2xl mb-8 max-w-3xl mx-auto font-mono">
                         a starter template for Next.js, builded with TailwindCSS, Shadcn UI, Lucia Auth, and Prisma.
                     </p>
                     <div className="flex gap-3 justify-center">
@@ -33,6 +35,7 @@ export default function Home() {
                     </div>
                 </div>
             </div>
+            <Footer/>
         </div>
     );
 }
