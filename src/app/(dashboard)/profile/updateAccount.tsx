86ab72c7dff3803b121bc21f2e8d9165b931547e
KhaiZulfa18 <khaizulfa18@gmail.com>
2024-10-30 22:40:27 +0700

    CHANGE THEMES

--- src/app/(dashboard)/profile/updateAccount.tsx
+++ src/app/(dashboard)/profile/updateAccount.tsx
@@ -49,7 +49,7 @@ export default function UpdateAccount({user} : {user: any}) {
                     <div className="relative inline-block">
                         {user?.isGithubConnected ? (
                             <>
-                            <Button variant="outline" className="gap-2 px-6 border border-green-500 dark:border-green-500 font-semibold text-green-700 dark:text-green-400 shadow-md hover:bg-green-50 dark:hover:bg-green-950 hover:text-green-800 dark:hover:text-green-300 hover:border-green-600  dark:hover:border-green-300 transition-all duration-300 ease-in-out">
+                            <Button variant="default" className="gap-2">
                                 <IconBrandGithub size={20} /> Connected Github
                             </Button>
                             <Button variant="outline" className="absolute -top-2 -right-2 w-6 h-6 p-0 rounded-full border-2 border-red-500 text-red-500 hover:bg-red-100 dark:hover:bg-red-900 dark:hover:text-red-400 hover:text-red-600 focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
@@ -59,7 +59,7 @@ export default function UpdateAccount({user} : {user: any}) {
                             </Button>
                             </>
                         ) : (
-                            <Button variant={'outline'} className="gap-2 px-6 border border-gray-800 dark:border-gray-300" asChild>
+                            <Button variant={'outline'} className="gap-2" asChild>
                                 <Link href="/signin/github?action=connect" prefetch={false}>
                                     <IconBrandGithub size={20} /> Connect to Github
                                 </Link>
@@ -69,7 +69,7 @@ export default function UpdateAccount({user} : {user: any}) {
                     <div className="relative inline-block">
                         {user?.isGoogleConnected ? (
                             <>
-                            <Button variant="outline" className="gap-2 px-6 border border-green-500 dark:border-green-500 font-semibold text-green-700 dark:text-green-400 shadow-md hover:bg-green-50 dark:hover:bg-green-950 hover:text-green-800 dark:hover:text-green-300 hover:border-green-600  dark:hover:border-green-300 transition-all duration-300 ease-in-out">
+                            <Button variant="default" className="gap-2 transition-all duration-300 ease-in-out">
                                 <IconBrandGoogle size={20} /> Connected Google
                             </Button>
                             <Button variant="outline" className="absolute -top-2 -right-2 w-6 h-6 p-0 rounded-full border-2 border-red-500 text-red-500 hover:bg-red-100 dark:hover:bg-red-900 dark:hover:text-red-400 hover:text-red-600 focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
@@ -79,7 +79,7 @@ export default function UpdateAccount({user} : {user: any}) {
                             </Button>
                             </>
                         ) : (
-                            <Button variant={'outline'} className="gap-2 px-6 border border-gray-800 dark:border-gray-300" asChild>
+                            <Button variant={'outline'} className="gap-2" asChild>
                                 <Link href="/signin/google?action=connect" prefetch={false}>
                                     <IconBrandGoogle size={20} /> Connect to Google
                                 </Link>
--- src/components/layout/content-layout.tsx
+++ src/components/layout/content-layout.tsx
@@ -6,7 +6,7 @@ export default function ContentLayout({children}: {children: React.ReactNode}) {
     return (
         <div>
             <NavbarDashboard/>
-            <div className={clsx('w-full min-h-screen bg-zinc-50 dark:bg-slate-950 px-1 py-2 md:px-10 md:py-5')}>
+            <div className={clsx('w-full min-h-screen bg-border dark:bg-secondary px-1 py-2 md:px-10 md:py-5')}>
                 {children}
             </div>
         </div>
